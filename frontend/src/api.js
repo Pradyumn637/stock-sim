c8155098916476f28b24e762495f332ae321d8e9
pradyumn637 <pradyumnnirvani0@gmail.com>
2026-04-07 14:39:39 +0530

    fix: python version + api update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://stock-sim-b0o8.onrender.com/',
 });
 
 API.interceptors.request.use((config) => {
